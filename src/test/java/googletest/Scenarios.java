7e75835532a1743acafc45bea35221b2778da8ab
santhoshmt <santhosh.kumar@zeotap.com>
2020-07-12 20:28:07 +0530

    chromeoptions

--- src/test/java/googletest/Scenarios.java
+++ src/test/java/googletest/Scenarios.java
@@ -2,6 +2,7 @@ package googletest;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
@@ -10,7 +11,15 @@ public class Scenarios {
     public void test(){
 
         System.setProperty("webdriver.chrome.driver",System.getProperty("chromeDriver"));
-        WebDriver driver = new ChromeDriver();
+        //WebDriver driver = new ChromeDriver();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("start-maximized"); // open Browser in maximized mode
+        options.addArguments("disable-infobars"); // disabling infobars
+        options.addArguments("--disable-extensions"); // disabling extensions
+        options.addArguments("--disable-gpu"); // applicable to windows os only
+        options.addArguments("--disable-dev-shm-usage"); // overcome limited resource problems
+        options.addArguments("--no-sandbox"); // Bypass OS security model
+        WebDriver driver = new ChromeDriver(options);
         driver.get("https://www.google.com");
         String expectedTitle = "Google";
         String actualTitle = driver.getTitle();
